873df0b5e153c2a70cf415e32463d2dc730d074e
horo_think <oo@oo.oo>
2016-01-11 22:40:31 +0300

    .

--- client/start.jsx
+++ client/start.jsx
@@ -1,17 +1,5 @@
 Meteor.startup(function () {
 
-    function mapStateToProps(state) {
-        log.debug('select state: {1}', state);
-        return {
-            taskCreator: state.taskCreator,
-            taskTable: state.taskTable,
-            idsIterateTaskCreator: state.idsIterateTaskCreator
-        }
-    }
-
-    //var AppRootWrap = connect(mapStateToProps)(TaskTableTest);
-
-    //ReactDOM.render(<RoutesWrap />, document.getElementById("container"));
     ReactDOM.render((
             <Routes />
     ), document.getElementById("container"));
